6a59c3cef96b762b3f2993e13e6388e8b174534e
ersinelmas <elmase23@itu.edu.tr>
2024-04-29 20:24:40 +0300

    Initial commit

---
+++ src/main/java/com/pair4/annotations/EnableSecurity.java
@@ -0,0 +1,14 @@
+package com.pair4.annotations;
+
+import org.springframework.context.annotation.ComponentScan;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@ComponentScan(basePackages = {"com.pair4"})
+public @interface EnableSecurity {
+}
